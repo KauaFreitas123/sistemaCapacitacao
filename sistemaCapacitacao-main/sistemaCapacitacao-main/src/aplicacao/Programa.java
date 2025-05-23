2e9c5d2cd703455043e51fd1c096f553d179b80b
KauaFreitas123 <kaua.freitas@estudante.ifmt.edu.br>
2025-05-23 12:12:20 -0300

    Add files via upload
    Sistema Capacitação Kauã

---
+++ sistemaCapacitacao-main/sistemaCapacitacao-main/src/aplicacao/Programa.java
@@ -0,0 +1,20 @@
+package aplicacao;
+
+import entidades.ServidorPublico;
+
+public class Programa {
+    public static void main(String[] args) {
+        ServidorPublico isabela = new ServidorPublico();
+        isabela.setNome("Isabela");
+        isabela.setCargo("Auditor");
+        isabela.setOrgao("ANVISA");
+        isabela.setLotacao("Brasilia");
+        isabela.setEmail("isabela@gmail.");
+
+        isabela.calcularSalarioHorasExtras(10, 5.60);
+
+        System.out.println("Servidor: "+ isabela.getNome());
+        System.out.println("Horas extras: R$ "+ isabela.getHorasExtras());
+
+    }
+}
