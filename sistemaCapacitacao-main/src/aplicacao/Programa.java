3d1ebf688b26942c14df5eea53d267a39991bfb5
KauaFreitas123 <kaua.freitas@estudante.ifmt.edu.br>
2025-06-06 11:01:23 -0400

    Programa Atualizado 060625

--- sistemaCapacitacao-main/src/aplicacao/Programa.java
+++ sistemaCapacitacao-main/src/aplicacao/Programa.java
@@ -1,20 +1,49 @@
 package aplicacao;
 
+import entidades.Produto;
 import entidades.ServidorPublico;
+
+import java.util.Scanner;
 
 public class Programa {
     public static void main(String[] args) {
-        ServidorPublico isabela = new ServidorPublico();
-        isabela.setNome("Isabela");
-        isabela.setCargo("Auditor");
-        isabela.setOrgao("ANVISA");
-        isabela.setLotacao("Brasilia");
-        isabela.setEmail("isabela@gmail.");
+        //ServidorPublico isabela = new ServidorPublico();
+       // isabela.setNome("Isabela");
+       // isabela.setCargo("Auditor");
+       // isabela.setOrgao("ANVISA");
+       // isabela.setLotacao("Brasilia");
+       // isabela.setEmail("isabela@gmail.");
 
-        isabela.calcularSalarioHorasExtras(10, 5.60);
+       // isabela.calcularSalarioHorasExtras(10, 5.60);
 
-        System.out.println("Servidor: "+ isabela.getNome());
-        System.out.println("Horas extras: R$ "+ isabela.getHorasExtras());
+       // System.out.println("Servidor: "+ isabela.getNome());
+       // System.out.println("Horas extras: R$ "+ isabela.getHorasExtras());
 
+        String nome;
+        int quant;
+        double preco;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Escreva os seguintes dados");
+        System.out.println("Nome do produto: ");
+        nome = scanner.nextLine();
+        System.out.println("Preço do produto: ");
+        preco = scanner.nextDouble();
+        System.out.println("Quantidade do produto: ");
+        quant = scanner.nextInt();
+
+        Produto produto = new Produto(nome, preco, quant);
+        produto.mostrarInfo();
+
+        System.out.println("Quantos produtos você deseja adicionar? ");
+        int quantMais = scanner.nextInt();
+        produto.adicionarProdutos(quantMais);
+        produto.mostrarInfo();
+
+        System.out.println("Quantos produtos você deseja remover? ");
+        int quantMenos = scanner.nextInt();
+        produto.removerProdutos(quantMenos);
+        produto.mostrarInfo();
     }
 }
